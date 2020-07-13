c8acedf1bc1b1c3410e74dd1eab531c562390bee
nurlimandiara <nurlimandiara@gmail.com>
2020-07-13 22:21:19 +0700

    change database to pgsql

--- models/setup.go
+++ models/setup.go
@@ -4,7 +4,7 @@ import (
 	"github.com/jinzhu/gorm"
 
 	// register the driver
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 // DB containts information for current DB connection
@@ -12,7 +12,7 @@ var DB *gorm.DB
 
 // ConnectDataBase = connecting to database
 func ConnectDataBase() {
-	db, err := gorm.Open("sqlite3", "test.db")
+	db, err := gorm.Open("postgres", "host=localhost port=5432 user=mypgadmin dbname=db0 password=vwxyz123")
 
 	if err != nil {
 		panic("Failed to connect to database!")
