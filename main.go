38f861332da122bce0458dac1096e49596bb89f4
nurlimandiara <nurlimandiara@gmail.com>
2020-07-15 23:38:20 +0700

    refactoring routes

--- main.go
+++ main.go
@@ -2,21 +2,14 @@ package main
 
 import (
 	"github.com/nurliman/Grasindo.API.go/models"
-
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"github.com/nurliman/Grasindo.API.go/routes"
 )
 
 func main() {
 
-	router := gin.Default()
-
-	router.GET("/", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{"data": "Hello World!"})
-	})
-
 	models.ConnectDataBase()
+
+	router := routes.SetupRouter()
 
 	router.Run()
 }
---
+++ routes/routes.go
@@ -0,0 +1,38 @@
+package routes
+
+import (
+	"net/http"
+
+	"github.com/nurliman/Grasindo.API.go/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+// SetupRouter function define routes endpoints
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+
+	router.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{"api": "grasindo"})
+	})
+
+	// endpoints "/v1"
+	v1 := router.Group("/v1")
+	{
+		v1.GET("/", func(context *gin.Context) {
+			context.JSON(http.StatusOK, gin.H{"api": "grasindo", "version": "1"})
+		})
+
+		// endpoints "/v1/customers"
+		customers := v1.Group("/customers")
+		{
+			customers.GET("/", controllers.AddCustomer)
+			customers.GET("/:id", controllers.AddCustomer)
+			customers.POST("/", controllers.AddCustomer)
+			customers.PUT("/:id", controllers.AddCustomer)
+			customers.DELETE("/:id", controllers.AddCustomer)
+		}
+	}
+
+	return router
+}
